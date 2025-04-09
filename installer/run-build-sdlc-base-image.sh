04e95c32e13d0d6a33ece21e5964bb3e10cc7e3d
Yitong <yitong@yitong.com>
2025-04-09 16:08:53 +0800

    refactor: change docker build shell

--- installer/run-build-sdlc-base-image.sh
+++ installer/run-build-sdlc-base-image.sh
@@ -3,4 +3,4 @@ cd ../
 docker rmi -f sdlc-python-pg:python3.11-pg15.8
 docker build -f installer/Dockerfile-python-pg -t sdlc-python-pg:python3.11-pg15.8 .
 docker rmi -f sdlc-vector-model:v1.0.1
-docker build -f Dockerfile-vector-model -t sdlc-vector-model:v1.0.1 .
+docker build -f installer/Dockerfile-vector-model -t sdlc-vector-model:v1.0.1 .
